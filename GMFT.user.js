50b9e6da8f83e356d451eaa88e24045fce30c94a
fg <106125159+forground@users.noreply.github.com>
2022-07-19 21:49:32 -0300

    Create GMFT.user.js

---
+++ GMFT.user.js
@@ -0,0 +1,80 @@
+(() => {
+    "use strict";
+
+    const icon = `
+        <svg class="octicon" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewbox="0 0 32 32">
+            <path d="M5.91 7.31v8.41c0 .66.05 1.09.14 1.31.09.21.23.37.41.48.18.11.52.16 1.02.16v.41H2.41v-.41c.5 0 .86-.05 1.03-.14.16-.11.3-.27.41-.5.11-.23.16-.66.16-1.3V11.7c0-1.14-.04-1.87-.11-2.2-.04-.26-.13-.42-.24-.53-.11-.1-.27-.14-.46-.14-.21 0-.48.05-.77.18l-.18-.41 3.14-1.28h.52v-.01zm-.95-5.46c.32 0 .59.11.82.34.23.23.34.5.34.82 0 .32-.11.59-.34.82-.23.22-.51.34-.82.34-.32 0-.59-.11-.82-.34s-.36-.5-.36-.82c0-.32.11-.59.34-.82.24-.23.51-.34.84-.34zm19.636 19.006h-3.39v-1.64h5.39v9.8h3.43v1.66h-9.18v-1.66h3.77v-8.16h-.02zm.7-6.44c.21 0 .43.04.63.13.18.09.36.2.5.34s.25.3.34.5c.07.18.13.39.13.61 0 .22-.04.41-.13.61s-.19.36-.34.5-.3.25-.5.32c-.2.09-.39.13-.62.13-.21 0-.43-.04-.61-.12-.19-.07-.35-.19-.5-.34-.14-.14-.25-.3-.34-.5-.07-.2-.13-.39-.13-.61s.04-.43.13-.61c.07-.18.2-.36.34-.5s.31-.25.5-.34c.17-.09.39-.12.6-.12zM2 30L27.82 2H30L4.14 30H2z"/>
+        </svg>`;
+   
+    // adicionar fonte monospace
+    function addMonospaceToggle() {
+        const button = document.createElement("button");
+
+        button.type = "button";
+        button.className = "ghd-monospace toolbar-item tooltipped tooltipped-n";
+        button.setAttribute("aria-label", "Toggle monospaced font");
+        button.setAttribute("tabindex", "-1");
+        button.innerHTML = icon;
+
+        $$(".toolbar-commenting").forEach(el => {
+            if (el && !$(".ghd-monospace", el)) {
+                el.insertBefore(button.cloneNode(true), el.childNodes[0]);
+            }
+        });
+    }
+   
+    function addBindings() {
+        $("body").addEventListener("click", event => {
+            let textarea, active;
+
+            const target = event.target;
+
+            if (target && target.classList.contains("ghd-monospace")) {
+                textarea = closest(".previewable-comment-form", target);
+                textarea = $(".comment-form-textarea", textarea);
+                textarea.classList.toggle("ghd-monospace-font");
+                textarea.focus();
+
+                active = textarea.classList.contains("ghd-monospace-font");
+
+                target.classList.toggle("text-blue", active);
+
+                return false;
+            }
+        });
+    }
+   
+    function $(str, el) {
+        return (el || document).querySelector(str);
+    }
+   
+    function $$(str, el) {
+        return Array.from((el || document).querySelectorAll(str));
+    }
+   
+    function closest(selector, el) {
+        while (el && el.nodeType === 1) {
+            if (el.matches(selector)) {
+                return el;
+            }
+            el = el.parentNode;
+        }
+        return null;
+    }
+   
+    // não inicializar se o GitHub Dark Script está ativo
+    if (!$("#ghd-menu")) {
+        GM_addStyle(`
+            .ghd-monospace-font { font-family: Menlo, Inconsolata, "Droid Mono",
+                monospace !important; font-size: 1em !important; }
+            .ghd-monospace > svg { pointer-events: none; }
+        `);
+   
+        document.addEventListener("ghmo:container", addMonospaceToggle);
+        document.addEventListener("ghmo:comments", addMonospaceToggle);
+    
+        addBindings();
+        
+        addMonospaceToggle();
+    }
+})();
